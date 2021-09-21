7df22d0792e8e8262eec0d2d736d23ce3ed9c412
Cristhian Oliver Martel Vicente <martelvicente1994@hotmail.com>
2021-09-21 10:42:54 -0500

    Primer cambio

--- cafeteria/Form1.Designer.vb
+++ cafeteria/Form1.Designer.vb
@@ -30,9 +30,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(130, 103)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "nombre"
+        Me.Label1.Text = "nombre nuevo"
         '
         'Form1
         '
